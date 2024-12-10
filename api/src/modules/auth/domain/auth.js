f39b2bf7806b51716a539c3d768d44cba2c0146a
Erwin Saludar <erwinsaludar@gmail.com>
2024-12-10 22:07:54 +0800

    update auto routes unit tests

--- api/src/modules/auth/domain/auth.js
+++ api/src/modules/auth/domain/auth.js
@@ -5,6 +5,12 @@ export default class Auth {
     throw new Error("Auth class cannot be instantiated");
   }
 
+  /**
+   * Hash password using bcrypt
+   *
+   * @param {string} password The password (in plain) to be hashed
+   * @returns {Promise<string>} The hashed password
+   */
   static async hashPassword(password) {
     if (!password || password.length <= 0)
       throw new Error("Password cannot be empty");
@@ -13,6 +19,13 @@ export default class Auth {
     return bcrypt.hash(password, salt);
   }
 
+  /**
+   * Verify password
+   *
+   * @param {string} password The password (in plain) to be verified
+   * @param {string} hashPassword The password (in hash) used for verification
+   * @returns {Promise<boolean>} True or false wether the passwords match
+   */
   static async verifyPassword(password, hashPassword) {
     if (!password || password.length <= 0)
       throw new Error("Password cannot be empty");
